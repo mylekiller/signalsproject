0c37ac227aef3e12a2f33fc8f1e4f13922c0e86d
Kyle Miller <kmille42@nd.edu>
2018-10-22 23:39:21 -0400

    First Four Measures Encoded as sine waves

---
+++ music.m
@@ -0,0 +1,12 @@
+c = 440*(2^((40-49)/12));
+g = 440*(2^((47-49)/12));
+a = 440;
+t = linspace(0,2,20000);
+wt = sin(2*pi*c*t).*(heaviside(t)-heaviside(t-0.20));
+wt = wt + sin(2*pi*c*t-0.25).*(heaviside(t-0.25)-heaviside(t-0.25-0.20));
+wt = wt + sin(2*pi*g*t-0.5).*(heaviside(t-0.5)-heaviside(t-0.5-0.20));
+wt = wt + sin(2*pi*g*t-0.75).*(heaviside(t-0.75)-heaviside(t-0.75-0.20));
+wt = wt + sin(2*pi*a*t-1).*(heaviside(t-1)-heaviside(t-1-0.20));
+wt = wt + sin(2*pi*a*t-1.25).*(heaviside(t-1.25)-heaviside(t-1.25-0.20));
+wt = wt + sin(2*pi*g*t-1.5).*(heaviside(t-1.5)-heaviside(t-1.5-0.45));
+sound(wt, 10000);
