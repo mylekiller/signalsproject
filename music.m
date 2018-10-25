70c0b66f2df83f96495b4419ffe386c7a963a8b9
Kyle Miller <kmille42@nd.edu>
2018-10-25 00:54:43 -0400

    Added Instrumentation Options

--- music.m
+++ music.m
@@ -1,9 +1,15 @@
 clear all
 clc
-%% Build Hash Table
+%% Build Hash Table and Harmoic Tables
 keyset = {'C','D','E','F','G','A','B'};
 valueset = [0,2,4,5,7,9,11];
 keys = containers.Map(keyset, valueset);
+piano = [1, 0.11, 0.34, 0.075, 0.065, 0.05, 0.01, 0.03];
+organ = [1, 0.8, 0.1, 0.4];
+flute = [1, 9, 3.8, 1.8, 0.4, 0.2];
+guitar = [1, 0.68, 1.26, 0.13, 0.13, 0.11, 0.01, 0.02, 0.2];
+oboe = [1, 0.98, 2.1, 0.19, 0.2, 0.23, 0.61, 0.3, 0.2];
+horn = [1, 0.39, 0.23, 0.22, 0.07, 0.05, 0.07, 0.05, 0.04, 0.03];
 
 %% Set other data
 samplingrate = 44100; % 44.1 kHz
@@ -66,7 +72,12 @@ for i = 1:numvoices
         dur = str2double(cell2mat(sp(3)));
         dur = dur * (1./tempo * 60);
         t = linspace(time, dur+time-.001, samplingrate*dur);
-        note = dynamic * sin(2*pi*freq*t);
+        z = 1;
+        note = zeros(1, length(t));
+        for k=1:length(horn)
+            note = note + (sin(2*pi*freq*k*t) * horn(k));
+        end
+        note = dynamic*note*0.1;
         arraylen = samplingrate*dur;
         envelope = [linspace(0,1,.02*samplingrate) linspace(1,.8,.02*samplingrate) linspace(.8,.7,arraylen-(.08*samplingrate)) linspace(.7,0,.04*samplingrate)];
         note = note .* envelope;
@@ -79,7 +90,9 @@ for i = 1:numvoices
     if isempty(fin)
         fin = zeros(1,length(song));
     end
-    fin = fin + song;
+    if i == 1
+        fin = fin + song;
+    end
     song = [];
     tline = fgetl(fid);
 end
