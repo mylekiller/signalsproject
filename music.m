9cb709194f63ab74dd7ec8242705cf34ebe9105b
Kyle Miller <kmille42@nd.edu>
2018-10-24 21:53:10 -0400

    Updated

--- music.m
+++ music.m
@@ -1,14 +1,42 @@
-% Simple Demo that needs lots of cleaning up, lots of adding things to get
-% a good grade, and lots of automation.
-c = 440*(2^((40-49)/12));
-g = 440*(2^((47-49)/12));
-a = 440;
-t = linspace(0,2,20000);
-wt = sin(2*pi*c*t).*(heaviside(t)-heaviside(t-0.20));
-wt = wt + sin(2*pi*c*t-0.25).*(heaviside(t-0.25)-heaviside(t-0.25-0.20));
-wt = wt + sin(2*pi*g*t-0.5).*(heaviside(t-0.5)-heaviside(t-0.5-0.20));
-wt = wt + sin(2*pi*g*t-0.75).*(heaviside(t-0.75)-heaviside(t-0.75-0.20));
-wt = wt + sin(2*pi*a*t-1).*(heaviside(t-1)-heaviside(t-1-0.20));
-wt = wt + sin(2*pi*a*t-1.25).*(heaviside(t-1.25)-heaviside(t-1.25-0.20));
-wt = wt + sin(2*pi*g*t-1.5).*(heaviside(t-1.5)-heaviside(t-1.5-0.45));
-sound(wt, 10000);
+clear all
+clc
+%% Build Hash Table
+keyset = {'C','D','E','F','G','A','B'};
+valueset = [0,2,4,5,7,9,11];
+keys = containers.Map(keyset, valueset);
+
+%% Read in music File and Build Sinusoids
+fid = fopen('music.txt');
+tline = fgetl(fid);
+sharp = false;
+flat = false;
+song = [];
+t = [];
+time = 0;
+while ischar(tline)
+    sp = strsplit(tline);
+    num = double(cell2mat(sp(1)));
+    note = char(num(1));
+    if length(num) == 2
+        if char(num(2)) == '#'
+            sharp = true;
+        elseif char(num(2)) == 'b'
+            flat = true;
+        end
+    end
+    keynum = (str2double(cell2mat(sp(2))) * 12) - 8 + keys(note);
+    if sharp
+        keynum = keynum + 1;
+    elseif flat
+        keynum = keynum - 1;
+    end
+    freq = 440*2^((keynum-49)/12);
+    dur = str2double(cell2mat(sp(3)));
+    t = linspace(time, dur+time, 1000/dur);
+    song = [song sin(2*pi*freq*t)];
+    time = time + dur;
+    sharp = false;
+    flat = false;
+    tline = fgetl(fid);
+end
+sound(song,1000)
